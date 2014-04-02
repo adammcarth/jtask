ea977fa219ca99c837374438a30213b3c27bcc0b
Adam McArthur <adam@adammcarthur.net>
2014-04-02 16:16:30 +1100

    add conversion method
    Gives users the ability to convert existing json files

---
+++ lib/jtask/convert.rb
@@ -0,0 +1,44 @@
+require "json"
+require "jtask/rename"
+require "jtask/save"
+
+class JTask
+  module Convert
+    def self.existing_json_file(filename, dir=nil)
+      # Check if user has specified a custom directory.
+      unless dir
+        # If not, a default folder is assigned.
+        if File.directory?("models/")
+          dir = "models/"
+        else
+          raise RuntimeError, "[JTask] The directory 'models/' doesn't exist in your current location. Please create it or refer to the documentation on how to change your file path."
+        end
+      end
+      original_file = File.read(File.join(dir, filename))
+      objects = JSON.parse(original_file)
+
+      output = Hash.new
+      objects.each_with_index do |(k, v), n|
+        output["#{n + 1}"] = {k => v}
+      end
+
+      # Rename the old version
+      begin
+        JTask.rename(filename, "#{filename}.old", dir)
+      rescue
+        raise RuntimeError, "[JTask] Failed to backup original file, conversion aborted."
+      end
+
+      # Write the new version
+      begin
+        JTask.save(filename, output, dir)
+      rescue
+        raise RuntimeError, "[JTask] Failed to save the converted file. The original can be found at '#{dir}#{filename}.old'"
+      end
+
+      return true
+    end
+  end
+end
+
+# JTask::Convert.existing_json_file("orders.json")
